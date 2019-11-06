eb5b481dd4c6867725987d46eafee6403c705445
unknown <alexepicity@gmail.com>
2019-11-06 14:39:37 -0400

    last commit

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-/* This is the function used to demonstrate the use a memoization in dynamic programming by sorting through the fibonacci sequence
-	I am aware that this isn't the most efficent way to sort through the fibonacci sequence but it's the one I'm demonstrating.
+/* 
+	This is the functions used to demonstrate the use of dynamic programming by sorting through the fibonacci sequence
  */
-
+ 
+/* up down method using memoization */
 var fibo = {1:1,2:1};
 
-function fibonacci(a){
+function up_down(a){
 	if(fibo[a] != null){
 		return fibo[a];
 	}else{
-		result = fibonacci(a-1) + fibonacci(a-2);
+		result = up_down(a-1) + up_down(a-2);
 		fibo[a] = result;
 		return result;
 	}
 }
 
+/* bottom up method (Most efficient method) */
+function bottom_up(n){
+	var a = 1;
+	var b = 1;
+	var c = 1;
+	for(i=2;i<n;i++){
+		a = c;
+		c = a+b;
+		b = a;
+	}
+	return c;
+}
 
 
 
@@ -22,11 +35,24 @@ function call(){
 	if(isNaN(input) || input <= 0){
 		document.getElementById("fib").innerHTML ="Please input a positive number";	
 	}else{
+		console.log("Up down method");
 		console.time("mytimer");
-		document.getElementById("fib").innerHTML = fibonacci(input);
+		document.getElementById("fib").innerHTML = up_down(input);
 		console.timeEnd("mytimer");
 		console.log(fibo);
 		fibo = {1:1,2:1};
 	}
 }
+function call_b(){
+	var input = document.getElementById("fibinput").value;
+	if(isNaN(input) || input <= 0){
+		document.getElementById("fib").innerHTML ="Please input a positive number";	
+	}else{
+		console.log("Bottom up method");
+		console.time("mytimer");
+		document.getElementById("fib").innerHTML = bottom_up(input);
+		console.timeEnd("mytimer");
+	}
+}
+
 
